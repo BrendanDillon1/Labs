382cc5b90528257ce576f88b8403cae908d5cbb6
Brendan Dillon <bdillon9@gmail.com>
2023-07-21 21:48:14 -0500

    java-refresher

---
+++ JavaScriptRefresher/script.js
@@ -0,0 +1,16 @@
+let firstname = "Brendan "
+let lastname = "Dillon"
+console.log(`${firstname} ${lastname}`);
+let num = 197416
+console.log(num);
+
+let multipliedpopulation = num * 3;
+console.log(multipliedpopulation);
+var javaScriptIsCool = true;
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+  }
+for (let i = 0; i <= 4; i++) {
+    console.log(i);
+  }
