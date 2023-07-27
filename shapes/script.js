717d87412fa51d9eaccc8f95dcfa39438849581e
Brendan Dillon <bdillon9@gmail.com>
2023-07-26 21:54:21 -0500

    shapes

---
+++ shapes/script.js
@@ -0,0 +1,19 @@
+function getAreaOfCircle(radius){
+    return Math.PI * radius**2
+}
+console.log(getAreaOfCircle(9))
+
+function getCircumferenceOfCircle(radius){
+    return 2 * Math.PI * radius
+}
+console.log(getCircumferenceOfCircle(9))
+
+function getAreaOfSquare(sides){
+    return sides**2
+}
+console.log(getAreaOfSquare(4))
+
+function getAreaOfTriangle(base ,height){
+    return .5 * (base * height)
+}
+console.log(getAreaOfTriangle(2, 4))
