b1380be2f958187cd8d944a8e1b8848761346dd5
Brendan Dillon <bdillon9@gmail.com>
2023-07-26 21:55:46 -0500

    counting

---
+++ counting/script.js
@@ -0,0 +1,36 @@
+for (let i = 1; i < 11; i++){
+    console.log(i)
+}
+for (let i = 10; i > 0; i--){
+    console.log(i)
+}
+
+let count = 1;
+ while (count <= 10) {
+    console.log(count);
+    count++;
+}
+
+
+count = 10;
+ while (count >= 1) {
+    console.log(count);
+    count--;
+}
+
+count = 1
+do {
+    console.log(count);
+    count++;
+} while (count <=10)
+
+count = 10
+do{
+    console.log(count);
+    count--;
+} while (count >=1)
+
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+for (const num of numbers ){
+    console.log(num)
+}
