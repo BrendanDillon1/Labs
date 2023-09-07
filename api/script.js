3b47ccc90a246f5eeaec5baf835268e957f0ff85
Brendan Dillon <bdillon9@gmail.com>
2023-09-06 19:26:41 -0500

    post links now work and are visable

--- api/script.js
+++ api/script.js
@@ -4,22 +4,24 @@ document.addEventListener("DOMContentLoaded", function () {
   fetch("https://www.reddit.com/r/aww/.json")
     .then((response) => response.json())
     .then((data) => {
-      let posts = data.data.children.slice(0, 10);
+      const posts = data.data.children.slice(1, 11);
 
       posts.forEach((post) => {
-        let postElement = document.createElement("div");
+        const postElement = document.createElement("div");
         postElement.className = "post";
 
-        let titleElement = document.createElement("div");
+        const titleElement = document.createElement("div");
         titleElement.className = "post-title";
         titleElement.innerText = post.data.title;
 
-        let thumbnailElement = document.createElement("img");
+        const thumbnailElement = document.createElement("img");
         thumbnailElement.src = post.data.thumbnail;
 
-        let linkElement = document.createElement("a");
+        const linkElement = document.createElement("a");
         linkElement.href = `https://www.reddit.com${post.data.permalink}`;
         linkElement.className = "post-link";
+        linkElement.target = "_blank";
+        linkElement.innerText = "view on Reddit";
 
         postElement.appendChild(titleElement);
         postElement.appendChild(thumbnailElement);
